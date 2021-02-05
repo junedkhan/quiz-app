6cb4c796d7074e06424d935a1bb35168bc10523a
junedkhan <junedkhan933@gmail.com>
2021-02-05 21:50:47 +0530

    added some responsiveness

--- src/components/BarChartSection/BarChartSection.js
+++ src/components/BarChartSection/BarChartSection.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, CartesianGrid, Legend, ResponsiveContainer } from 'recharts';
 import './BarChartSection.css';
 
 const BarChartSection = ({ chartData }) => {
     return (
         <div className="barchart-section-container">
             <h1>Result</h1>
+            <ResponsiveContainer >
             <BarChart
-                width={500}
-                height={300}
                 data={chartData}
-                margin={{
-                    top: 5, right: 30, left: 20, bottom: 5,
-                }}
             >
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="key" />
@@ -21,6 +17,7 @@ const BarChartSection = ({ chartData }) => {
                 <Legend />
                 <Bar dataKey="answers" fill="#8884d8" />
             </BarChart>
+</ResponsiveContainer>
         </div>
     );
 }
